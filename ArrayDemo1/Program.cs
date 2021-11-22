bb16b7e4623f3c628719b6270793bec7671a91f7
Tae <wuhtangclan36@gmail.com>
2021-11-22 14:51:43 -0500

    added

--- ArrayDemo1/Program.cs
+++ ArrayDemo1/Program.cs
@@ -6,7 +6,45 @@ namespace ArrayDemo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] { 5, 10, 15, 20, 25 };
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("Now let's make a string array");
+            string[] names = new string[] { "Fred", "Sally", "Julia", "Sam" };
+            Console.WriteLine(names[0]); //print out the first one
+            Console.WriteLine(names[2]); //print out the third one
+
+            Console.WriteLine("change one and print them all out");
+            //Let's suppose we need to change one
+            //Let's change Same to Samantha
+            names[3] = "Samantha";
+            for (int i=0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            //Let's create an array but not initialize it. We do, however,
+            //have to state the size when we create it.
+            //We'll create an aray with four slots, but initially they 
+            //are empty
+
+            Console.WriteLine("create an array without initialzing it");
+            string[] words = new string[4];
+            words[0] = "apple";
+            words[1] = "banana";
+            words[2] = "Orange";
+            words[3] = "watermelon";
+
+            for (int i =0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+
+
         }
     }
 }
---
+++ ArrayDemo2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ArrayDemo2
+{
+    class Program
+    {
+        static void PrintMenu(string[] theItems, decimal[] thePrices)
+        {
+            Console.WriteLine("Here is our menu:");
+            for (int i = 0; i < theItems.Length; i++)
+            {
+                Console.WriteLine($"{theItems[i],-12}\t{thePrices[i],-6}");  //,-12 and ,-6 makes the alignments better
+            }
+        }
+
+        static int FindItem(string[] theItems, string search)
+        {
+            for (int i = 0; i< theItems.Length; i++)
+            {
+                if (theItems[i].ToLower() == search.ToLower())
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+             *    create a menus used at a coffee house
+             *          item        price
+             *          ----        -----
+             *          coffee      2.00
+             *          espresso    2.50
+             *          muffin      3.50
+             *          croissant    4.00
+             */
+
+            //For now we're creating two separate arrays
+            string[] items = new string[] { "coffee", "espresso", "muffin", "croissant" };
+            decimal[] prices = new decimal[] { 2.00m, 2.50m, 3.50m, 4.00m };
+
+            PrintMenu(items, prices);
+
+            //Console.WriteLine("Here is our menu:");
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //    Console.WriteLine($"{items[i],-12}\t{prices[i],-6}");  //,-12 and ,-6 makes the alignments better
+            //}
+
+            Console.WriteLine("Please make a selection:");
+            string entry = Console.ReadLine();
+            int index = FindItem(items, entry);
+            
+
+            if (index > -1)
+            {
+                Console.WriteLine($"The price of {items[index]} is {prices[index]}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, we do not have {entry}.");
+            }
+
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //    if (items[i].ToLower() == entry.ToLower())
+            //    {
+            //        Console.WriteLine($"The price of {items[i]} is {prices[i]}"); //can swap out {entry} for {itmes[i]}. it is the same
+            //        break;
+               
+           
+        }
+    }
+}
