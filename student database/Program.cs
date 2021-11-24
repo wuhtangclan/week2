6dc007f1aaf655380bbeb37120e1bcc7d9f6d005
Tae <wuhtangclan36@gmail.com>
2021-11-23 20:57:22 -0500

    update

--- student database/Program.cs
+++ student database/Program.cs
@@ -4,15 +4,23 @@ namespace student_database
 {
     class Program
     {
-        //static int FindName (string [] studentNames, string search)
-        //{
-        //    for (int i = 0; i < studentNames.Length; i++)
-        //    {
-        //        if (studentNames[i] == search)
-        //            return i;
-        //    }
-        //    return -1;
-        //}
+        static bool GoAgain(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string entry = Console.ReadLine();
+                if (entry.ToLower() == "n")
+                {
+                    return false;
+                }
+                if (entry.ToLower() == "y")
+                {
+                    return true;
+                }
+                Console.WriteLine("Im sorry please enter a valid entry");
+            }
+        }
 
 
         static void Main(string[] args)
@@ -21,57 +29,53 @@ namespace student_database
             string[] homeTown = new string[] { "Seoul", "Royal Oak", "Troy", "Grosse Pointe" };
             string[] favoriteFood = new string[] { "Jerk Chicken", "Spam and Egg", "Pizza", "Burger" };
 
-            Console.Write("Greetings! Which student would you like to learn more about? Enter a number 1-4: ");
-            string entry = Console.ReadLine();
-            int index = int.Parse(entry);
-            //string result = name[num];
-            //Console.WriteLine(result);
-            //  The above uses two lines to accomplish
-            //  what the line below does in one line.
-            if(index > -1 && index < 4)
+            do
             {
-                Console.WriteLine($"Student {index} is {name[index]}.");
-                Console.WriteLine("What would you like to know? Enter 'hometown' or 'favorite food': ");
-                entry = Console.ReadLine();
-                if (entry.ToLower() == "hometown")
+                string entry = "";
+                int index = 0;
+                bool validEntry = false;
+
+                do
                 {
-                    Console.WriteLine($"{name[index]} is from {homeTown[index]}");
-                }
-                else if (entry.ToLower() == "favorite food")
+                    Console.Write("Greetings! Which student would you like to learn more about? Enter a number 1-4: ");
+                    entry = Console.ReadLine();
+                    index = int.Parse(entry);
+
+                    if (index >= 1 && index <= name.Length)
+                    {
+                        validEntry = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Please enter a number between 1 and {name.Length}");
+                    }
+                } while (validEntry == false);
+
+                index--;
+                Console.Write($"Student {index + 1} is {name[index]}. ");
+                validEntry = false;
+
+                do
                 {
-                    Console.WriteLine($"{name[index]} loves {favoriteFood[index]}");
-                }
-                else
-                {
-                    Console.WriteLine("That category does not exist.");
-                }
+                    Console.Write("What would you like to know? Enter 'hometown' or 'favortie food': ");
+                    entry = Console.ReadLine().ToLower();
 
-            }
-            else
-            {
-                Console.WriteLine("Please provide a valid entry");
-            }
-
-            //Console.WriteLine("What would you like to know? Enter 'hometown' or 'favorite food': ");
-            //entry = Console.ReadLine();
-            //if (entry.ToLower() == "hometown")
-            //{
-            //    Console.WriteLine($"{name[index]} is from {homeTown[index]}");
-            //}
-            //else if (entry.ToLower() =="favorite food")
-            //{
-            //    Console.WriteLine($"{name[index]} loves {favoriteFood[index]}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("That category does not exist.");
-            //}
-            
-
-
-            Console.WriteLine(homeTown[index]);
-            Console.WriteLine(favoriteFood[index]);
-           
+                    if (entry == "hometown")
+                    {
+                        Console.WriteLine($"{name[index]} is from {homeTown[index]}");
+                        validEntry = true;
+                    }
+                    else if (entry == "favorite food")
+                    {
+                        Console.WriteLine($"{name[index]}'s favorite food is {favoriteFood[index]}");
+                        validEntry = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid entry. Please enter hometown or favorite food");
+                    }
+                } while (validEntry == false);
+            } while (GoAgain("Would you like to learn about another student? (y/n)"));
         }
     }
 }
